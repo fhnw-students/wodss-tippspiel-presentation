62ade587bb21b148267b97278d60e5b0ba5878e9
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-04-06 07:15:40 +0200

    Add first draft

--- src/app.js
+++ src/app.js
@@ -11,31 +11,40 @@ import fontawesome from '@fortawesome/fontawesome';
 import fontawesomeSolid from '@fortawesome/fontawesome-free-solid';
 fontawesome.library.add(fontawesomeSolid);
 
-import pageJS from './slides/000-page-js.slide';
-import pageHTML from './slides/000-page-html.slide.html';
-import pageMarkDown from './slides/000-page-md.slide.md';
-import pageIcons from './slides/000-page-icons.slide.html';
+import titleSlide from './slides/000-title.slide.html';
+import indexSlide from './slides/001-index.slide.html';
 
-// Star Wars Theme
-import swLead from './slides/100-sw-lead.slide.html';
-import swTitle from './slides/100-sw-title.slide.html';
-import swOpening from './slides/100-sw-opening.slide.html';
-import swGif from './slides/100-sw-gif.slide.html';
+import ermSlide from './slides/010-erm.slide.html';
+import uiRegisterSlide from './slides/100-ui-register.slide.html';
+import uiResetSlide from './slides/101-ui-reset.slide.html';
+import uiLoginSlide from './slides/102-ui-login.slide.html';
+import uiGamesSlide from './slides/103-ui-games.slide.html';
+import uiRankingSlide from './slides/104-ui-ranking.slide.html';
+import uiTeamSlide from './slides/105-ui-team.slide.html';
+import uiProfileSlide from './slides/106-ui-profile.slide.html';
+
+import questionSlide from './slides/900-questions.slide.html';
+import emptySlide from './slides/999-empty.slide.html';
 
 // -------------------------------------------------------------------------
 // Slides
 // -------------------------------------------------------------------------
 
 const slides = [
-  swLead,
-  swTitle,
-  swOpening,
-  swGif,
+  titleSlide,
+  indexSlide,
 
-  pageJS,
-  pageHTML,
-  pageMarkDown,
-  pageIcons,
+  ermSlide,
+  uiRegisterSlide,
+  uiResetSlide,
+  uiLoginSlide,
+  uiGamesSlide,
+  uiRankingSlide,
+  uiTeamSlide,
+  uiProfileSlide,
+
+  questionSlide,
+  emptySlide,
 ]
   .forEach(s => $('#slides').append(s));
 
